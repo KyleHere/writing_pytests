8c1e8a2f5b87492067cb8bd16f37c241180b00f1
kyle <ktseng122@gmail.com>
2021-08-02 16:59:04 -0400

    test 9 complete

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,5 +1,5 @@
 def parse(value):
-    romans = ["I","II","III","IV","V","VI","VII","VIII"]
+    romans = ["I","II","III","IV","V","VI","VII","VIII", "IX"]
     for n in romans:
         if n == value:
             return romans.index(n) + 1
