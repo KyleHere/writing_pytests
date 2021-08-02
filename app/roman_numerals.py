fcae7be8b3cc9129ec915582fb63e57fdcd51d1b
Manna Kong <m.kong.57@gmail.com>
2021-08-02 15:53:22 -0500

    code refactor

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,17 +1,5 @@
 def parse(value):
-    if value == "I":
-        return 1
-    elif value == "II":
-        return 2
-    elif value == "III":
-        return 3
-    elif value == "IV":
-        return 4
-    elif value == "V":
-        return 5
-    elif value == "VI":
-        return 6
-    elif value == "VII":
-        return 7
-    elif value == "VIII":
-        return 8
+    romans = ["I","II","III","IV","V","VI","VII","VIII"]
+    for n in romans:
+        if n == value:
+            return romans.index(n) + 1
