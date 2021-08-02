05769cb0f2a48704325fa8c41a86191de06601e2
Manna Kong <m.kong.57@gmail.com>
2021-08-02 16:14:06 -0500

    9 and 10

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,5 +1,5 @@
 def parse(value):
-    romans = ["I","II","III","IV","V","VI","VII","VIII", "IX"]
+    romans = ["I","II","III","IV","V","VI","VII","VIII", "IX","X"]
     for n in romans:
         if n == value:
             return romans.index(n) + 1
--- test/test_roman_numerals_pytest.py
+++ test/test_roman_numerals_pytest.py
@@ -1,6 +1,14 @@
+from pytest import mark
 from app.roman_numerals import parse
 
-def test_roman_numeral_parser():
-    value = parse("IX")
 
-    assert value == 9
+# def test_roman_numeral_parser():
+#     value = parse("IX")
+
+#     assert value == 9
+
+@mark.parametrize('s, expected', [("IX", 9), ("X", 10)])
+def test_roman_numeral_parser(s, expected):
+    result = parse(s)
+
+    assert result == expected
